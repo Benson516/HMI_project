47714a911239f2fcfcea376859dd3b40b0ebafda
benson516 <benson516@hotmail.com>
2019-08-11 21:54:56 +0800

    Update gTTS_generator.py

--- scripts/gTTS_generator.py
+++ scripts/gTTS_generator.py
@@ -3,8 +3,16 @@
 # ^^^ The above line specify the encoding of the unicode string (u"__some_unicode_string__") in python
 
 from gtts import gTTS
-from io import BytesIO
-import time
+
+file_list = list()
+en_list = list()
+zh_list = list()
+#-------------------------------#
+# Turn right
+
+
+#-------------------------------#
+
 
 
 mp3_fp = BytesIO()
@@ -13,26 +21,3 @@ tts = gTTS('Hello, this is a Google tts directly from your computer.')
 
 # Save to file
 tts.save("test.mp3")
-# Stream to play
-tts.write_to_fp(mp3_fp)
-
-
-mp3_fp.seek(0) # Important! Return the pointer to the head
-
-"""
-# The following code does not work
-from playsound import playsound
-playsound(mp3_fp)
-"""
-
-# pygame works; however, the CPU loading is quit high
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load(mp3_fp)
-
-
-while True:
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        time.sleep(0.1)
-#
